fdb1a3d3cd86db67a208758a66af34f277a8eccd
BuildTools <unconfigured@null.spigotmc.org>
2021-03-09 21:35:48 -0300

    1.2

---
+++ 04-Struct-vs-Class/RectApp.java
@@ -0,0 +1,33 @@
+public class RectApp {
+    public static void main(String[] args) {
+        Rect r1 = new Rect(1, 101, 10, 10);
+        r1.print();
+        r1.area();
+        r1.drag(100, -100);
+        r1.print();
+    }
+}
+class Rect {
+    int x, y, w, h;
+
+    Rect(int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    void print() {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    int area() {
+        return this.w * this.h;
+    }
+
+    void drag(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+}
